489d234cd3afa0619421f234206edaedca6ce25c
Hale Deng <haledeng88@gmail.com>
2019-03-23 10:26:15 +0800

    add commnet

--- src/core/global-api/mixin.js
+++ src/core/global-api/mixin.js
@@ -2,9 +2,9 @@
 
 import { mergeOptions } from '../util/index'
 
-export function initMixin (Vue: GlobalAPI) {
-  Vue.mixin = function (mixin: Object) {
-    this.options = mergeOptions(this.options, mixin)
+export function initMixin(Vue: GlobalAPI) {
+  Vue.mixin = function(mixin: Object) {
+    this.options = mergeOptions(this.options, mixin) // ! 合并 mixin 的配置
     return this
   }
 }
--- src/core/global-api/use.js
+++ src/core/global-api/use.js
@@ -2,22 +2,25 @@
 
 import { toArray } from '../util/index'
 
-export function initUse (Vue: GlobalAPI) {
-  Vue.use = function (plugin: Function | Object) {
-    const installedPlugins = (this._installedPlugins || (this._installedPlugins = []))
+export function initUse(Vue: GlobalAPI) {
+  Vue.use = function(plugin: Function | Object) {
+    const installedPlugins =
+      this._installedPlugins || (this._installedPlugins = []) // ! 维护一个数组，存储注册后的插件
     if (installedPlugins.indexOf(plugin) > -1) {
       return this
     }
 
     // additional parameters
     const args = toArray(arguments, 1)
-    args.unshift(this)
+    args.unshift(this) // ! 把 Vue 添加到参数数组最前面
+    // ! 查找 install 方法，并调用
     if (typeof plugin.install === 'function') {
-      plugin.install.apply(plugin, args)
+      plugin.install.apply(plugin, args) // ! 第一个传入的参数是 Vue，下同
+      // ! 插件本身是函数的，也可以直接调用
     } else if (typeof plugin === 'function') {
       plugin.apply(null, args)
     }
-    installedPlugins.push(plugin)
+    installedPlugins.push(plugin) // !把插件放入维护的数组
     return this
   }
 }
